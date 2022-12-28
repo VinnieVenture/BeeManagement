fed4a827d3b804fc874a73e09d1d209b8888e057
VinnieVenture <vinventurewow@gmail.com>
2022-12-28 22:54:13 +0100

    bee management

--- BeeManagement/MainWindow.xaml.cs
+++ BeeManagement/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeeManagement.Bees;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,24 @@ namespace BeeManagement
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Queen queen;
         public MainWindow()
         {
             InitializeComponent();
+            queen = new Queen();
+            statusReport.Text = queen.StatusReport;
+        }
+
+        private void AssignJob_Click(object sender, RoutedEventArgs e)
+        {
+            queen.AssignBee(jobSelector.Text);
+            statusReport.Text = queen.StatusReport;
+        }
+
+        private void WorkShift_Click(object sender, RoutedEventArgs e)
+        {
+            queen.WorkTheNextShift();
+            statusReport.Text = queen.StatusReport;
         }
     }
 }
